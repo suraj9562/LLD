7fcb3dc2c13d3851f24f21ce77ef75321fcc49d4
@suraj05 <wadikarsuraj02@gmail.com>
2024-12-09 21:17:05 +0530

    ADDED : COR design pattern

---
+++ COR/ConsoleBasedLogger.java
@@ -0,0 +1,10 @@
+public class ConsoleBasedLogger extends Logger {
+    public ConsoleBasedLogger(int levels) {
+        this.levels = levels;
+    }
+
+    @Override
+    protected void displayLogInfo(String msg) {
+        System.out.println("CONSOLE LOGGER INFO: " + msg);
+    }
+}
